3f0fba0f99b662dcdcddc8255aaf97678c6b049c
Kai Florian Wessela <kai@wessela.eu>
2019-07-26 17:59:44 +0200

    clearups, account comments

--- api/account.php
+++ api/account.php
@@ -1,13 +1,16 @@
 <?php
+# CLASS > Account
+# This is the account class. Account explains itself.
+
 class Account {
-	private $uuid;
-	private $username;
-	private $email;
-	private $pwhash;
+	private $uuid; # uuid of the account, randomly generated
+	private $username; # username of the account / user
+	private $email; # email of the account / user
+	private $pwhash; # hashed password of the account
 
-	private $exists = false;
+	private $exists = false; # true = account exists on database / false = account doesnt exist on database
 
-	global $pdo;
+	global $pdo; # reference to database connection connection
 
 	public function __construct($uuid, $username, $email, $pwhash) {
 		$this->uuid = $uuid;
@@ -16,36 +19,41 @@ class Account {
 		$this->pwhash = $pwhash;
 	}
 
-	public static function pull($value, $key = 'uuid') { // load account from database and return it
-		if($key == 'uuid'){
+	# get account from db and return it
+	public static function pull($value, $key = 'uuid') {
+		if($key == 'uuid'){ # get account by uuid (default)
 			$s = $pdo->prepare('SELECT * FROM accounts WHERE uuid = ?');
-		} else if($key == 'username'){
+		} else if($key == 'username'){ # get account by username
 			$s = $pdo->prepare('SELECT * FROM accounts WHERE username = ?');
-		} else if($key == 'email'){
+		} else if($key == 'email'){ # get account by email
 			$s = $pdo->prepare('SELECT * FROM accounts WHERE email = ?');
-		} else {
-			return false;
 		}
 
 		$s->execute(array($value));
 
-		if($s->rowCount() == 1){
+		if($s->rowCount() == 1){ # check if there is a result from the database request
+			# there is one, create account and return it
 			$r = $s->fetch();
 			$acc = new Account($r['uuid'], $r['username'], $r['email'], $r['pwhash']);
 			$acc->exists(true);
+			return $acc;
 		} else {
+			# there is no result
 			return false;
 		}
 	}
 
+	# create new account and return it - only use if account doesnt exist already - generates some values
 	public static function new($username, $email, $password) {
 		return new Account(self::generateUUID(), $username, $email, self::generateHash($password));
 	}
 
+	# check if a password is correct for the account
 	public function verify($password) {
 		return password_verify($password, $this->pwhash);
 	}
 
+	# push account to database, update or insert
 	public function push() {
 		if($this->exists()){
 			$s = $pdo->prepare('UPDATE accounts SET username = :username, email = :email, pwhash = :pwhash WHERE uuid = :uuid');
@@ -56,7 +64,8 @@ class Account {
 		$s->execute(array('uuid' => $this->uuid, 'username' => $this->username, 'email' => $this->email, 'pwhash' => $this->pwhash));
 	}
 
-	private function exists($set) { // check if account exists on database or set existance
+	# check if account exists or set this value
+	private function exists($set) {
 		if($set == true){
 			$this->exists = true;
 		} else if($set == false){
@@ -66,12 +75,14 @@ class Account {
 		}
 	}
 
+	# generate an uuid
 	public static function generateUUID() {
-		$hex = bin2hex(random_bytes(16));
-		$chu = explode(' ', chunk_split($hex, 4, ' '));
-		return $chu[0].$chu[1].'-'.$chu[2].'-'.$chu[3].'-'.$chu[4].'-'.$chu[5].$chu[6].$chu[7];
+		$hex = bin2hex(random_bytes(16)); # generate 16 random bytes and convert them to a hexadecimal string
+		$chu = explode(' ', chunk_split($hex, 4, ' ')); # split that string to 4 chars long chunks
+		return $chu[0].$chu[1].'-'.$chu[2].'-'.$chu[3].'-'.$chu[4].'-'.$chu[5].$chu[6].$chu[7]; # return and set -
 	}
 
+	# generate a hash of a given password
 	public static function generateHash($password) {
 		return password_hash($password, PASSWORD_DEFAULT);
 	}
--- api/config.php
+++ api/config.php
@@ -1,11 +1,6 @@
 <?php
-# version: 0.0.1
-
 define('DB_HOST', '');
 define('DB_NAME', '');
 define('DB_USER', '');
 define('DB_PASSWORD', '');
-
-define('USE_HIBP_CHECK', true);
-define('FORCE_HIBP_SUCCESS', false);
 ?>
