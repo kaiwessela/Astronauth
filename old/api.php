ac1815a7a24a1e1dbfd625e7169e45ed79da6e83
Kai Florian Wessela <kai@wessela.eu>
2020-05-26 22:15:17 +0200

    cleanup

--- old/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-
-require('api/main.php');
-
-
-?>
--- old/api/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'astronauth');
-define('DB_USER', 'astro');
-define('DB_PASSWORD', 'FGWbBWvl4bVxbDbC');
-?>
