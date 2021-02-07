e62f940d984186ca358a2a4b7089ea15f87b89fb
Kai Florian Wessela <kai@wessela.eu>
2021-02-07 15:21:27 +0100

    add composer

--- build.sh
+++ build.sh
@@ -12,3 +12,9 @@ mkdir build/libs/Astronauth
 # copy
 cp -r frontend/. build/astronauth
 cp -r core/. build/libs/Astronauth
+
+
+# copy fonts
+cp vendor/redhatofficial/overpass/webfonts/overpass-webfont/overpass-regular.woff2 build/astronauth/resources/fonts/
+cp vendor/redhatofficial/overpass/webfonts/overpass-webfont/overpass-italic.woff2 build/astronauth/resources/fonts/
+cp vendor/redhatofficial/overpass/webfonts/overpass-webfont/overpass-bold.woff2 build/astronauth/resources/fonts/
